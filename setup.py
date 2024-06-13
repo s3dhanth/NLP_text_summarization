daacb1a8b021e170ed5ce4885fdd31ed7f3df04f
s3dhanth <sidhantgarg455@gmail.com>
2024-06-14 00:14:21 +0530

    requirements & installation

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "NLP_text_summarization"
+AUTHOR_USER_NAME = "s3dhanth"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "sidhantgarg455@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small custom python package for my NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
