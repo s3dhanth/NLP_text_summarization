e96ee92d7f49fb671568b2dd4da984a8688b6e6d
s3dhanth <sidhantgarg455@gmail.com>
2024-07-07 15:10:29 +0530

    data ingestion notebook added

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## workflow
+
+1. update config.yaml
+2. update params.yaml
+3. update entity
+4. update configuration manager in scr config
+5. update the components
+6. upadte the pipeline
+7. update the main.py 
+8. update the app.py
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,11 @@
+## entity is return type of the function
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen= True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+    
