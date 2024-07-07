cf6652ee20c36b6a7f1da65649a5357cc3654554
s3dhanth <sidhantgarg455@gmail.com>
2024-07-08 00:10:51 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
+from textSummarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+   Data_ingestion = DataIngestionTrainingPipeline()
+   Data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+
+TAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   Data_validation = DataValidationTrainingPipeline()
+   Data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -3,7 +3,7 @@ from textSummarizer.utils.common import read_yaml, create_directories
 
 ## import dataingestionconfig function also and config_file_path so we have use this function
 
-from textSummarizer.entity import (DataIngestionConfig)
+from textSummarizer.entity import (DataIngestionConfig,DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -27,3 +27,16 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
