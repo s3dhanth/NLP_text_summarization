9a1b786085987de215faa211eb0b495a32f66349
s3dhanth <sidhantgarg455@gmail.com>
2024-07-15 18:00:49 +0530

    prediction UI

---
+++ src/textSummarizer/pipeline/prediction.py
@@ -0,0 +1,27 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+# FIRST WE have to load the model and its tokenizer
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
+        # as model evaluation contains the model path and model tokenizer
+    
+    def predict(self,text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+
+
+        pipe = pipeline("summarization", model= self.config.model_path, tokenizer=tokenizer)
+
+        ## 
+        print("Dialogue:")
+        print(text)
+        output = pipe(text, **gen_kwargs)[0]['summary_text']
+        print("\nModel Summary:")
+        print(output)
+
+        return output
+        
